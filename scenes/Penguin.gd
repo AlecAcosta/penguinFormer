cada2ba70f386091d11e1d6359f502bba04601cf
AlecAcosta <64831347+AlecAcosta@users.noreply.github.com>
2021-04-19 18:59:07 -0500

    initial commit

---
+++ scenes/Penguin.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+var acceleration : float = 256
+var gravity : float = 384
+var jumpForce : float = 128
+var maxHorSpeed : float = 96
+var maxVerFallingSpeed : float = 128
+var frictionWeight : float = 0.2
+
+var movement : Vector2 = Vector2(0,0)
+
+
+func _physics_process(delta):
+	var horDir : int = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	movement.x += horDir * acceleration * delta
+	movement.x = clamp(movement.x,-maxHorSpeed,maxHorSpeed)
+	
+	if (horDir == 0):
+		if (abs(movement.x) < 0.1):
+			movement.x = 0
+		else:
+			movement.x = lerp(movement.x,0,frictionWeight)
+	else:
+		$Sprite.scale.x = horDir
+	
+	movement.y += gravity * delta
+	movement.y = clamp(movement.y,movement.y,maxVerFallingSpeed)
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		movement.y = -jumpForce
+	
+	movement = move_and_slide(movement,Vector2.UP)
+	
+	var animation : String = "idle"
+	
+	if (horDir != 0):
+		animation = "run"
+	if (! is_on_floor()):
+		if (movement.y < 0):
+			animation = "jump"
+		else:
+			animation = "fall"
+	
+	$AnimationPlayer.play(animation)
