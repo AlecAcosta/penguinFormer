b04f5298428e9124769388ebac59b4f90c21797b
AlecAcosta <64831347+AlecAcosta@users.noreply.github.com>
2021-04-24 12:21:04 -0500

    changed coin sprites

--- scenes/scripts/mainCamera.gd
+++ scenes/scripts/mainCamera.gd
@@ -8,5 +8,5 @@ onready var levelLenght = $"../ColorRect_lvlBkg".rect_size.x
 onready var viewportWidth = get_viewport_rect().size.x
 
 func _physics_process(delta):
-	if (player.global_position.x > global_position.x and global_position.x < levelLenght- (viewportWidth/2)):
+	if (player.global_position.x > global_position.x and global_position.x < levelLenght- (viewportWidth/2)-1):
 		global_position.x = player.global_position.x
