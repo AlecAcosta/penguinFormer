a8fbea4199aa5cd628fa19f5acb1cc2516d89444
AlecAcosta <64831347+AlecAcosta@users.noreply.github.com>
2021-04-24 00:57:37 -0500

    coin animation when breaking blocks

--- scenes/scripts/Box.gd
+++ scenes/scripts/Box.gd
@@ -1,11 +1,14 @@
 extends StaticBody2D
 
 var rCoinGrabbed = preload("res://scenes/CoinGrabbed.tscn")
+var dropCoin = true
 
 func punched():
-	var iCoinGrabbed = rCoinGrabbed.instance()
-	iCoinGrabbed.global_position = self.global_position #+ Vector2(0,-16)###
-	get_tree().get_root().add_child(iCoinGrabbed)
-	$AnimationPlayer.play("break")
-	yield($AnimationPlayer,"animation_finished")
-	queue_free()
+	if (dropCoin):
+		dropCoin = false
+		var iCoinGrabbed = rCoinGrabbed.instance()
+		iCoinGrabbed.global_position = self.global_position + Vector2(0,-8)###
+		get_tree().get_root().add_child(iCoinGrabbed)
+		$AnimationPlayer.play("break")
+		yield($AnimationPlayer,"animation_finished")
+		queue_free()
