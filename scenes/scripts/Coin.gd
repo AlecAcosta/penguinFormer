e2f7fbd439cab16eb8cf015dd7334bedc45d462f
AlecAcosta <64831347+AlecAcosta@users.noreply.github.com>
2021-05-25 02:49:57 -0500

    added grabbedCoin to normal coins
    also added coins to the world

--- scenes/scripts/Coin.gd
+++ scenes/scripts/Coin.gd
@@ -1,5 +1,14 @@
 extends Area2D
 
+var rCoinGrabbed = preload("res://scenes/CoinGrabbed.tscn")
+var dropCoin = true
+
 func _on_body_entered(body):
 	if (body.name == "Penguin"):
 		queue_free()
+	if (dropCoin):
+		dropCoin = false
+		var iCoinGrabbed = rCoinGrabbed.instance()
+		iCoinGrabbed.global_position = self.global_position + Vector2(0,-8)
+		get_tree().get_root().add_child(iCoinGrabbed)
+		Global.levelCoins += 1
